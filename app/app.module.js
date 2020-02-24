09ce96b19241891022fa1d1f0a4bf63222756f42
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-24 12:07:03 -0800

    Got the basic file structure needed to start, next is to figure out how to connect the components together

---
+++ app/app.module.js
@@ -0,0 +1,13 @@
+var myApp = angular.module('myApp', ['ngRoute']); 
+
+myApp.config(['$routeProvider', function($routeProvider) {
+}]);
+
+myApp.directive('randomPerson',['$http', function($http) { 
+
+}])
+
+myApp.controller('AppController',['$scope', '$http', function($scope, $http) {
+
+        
+}]);
