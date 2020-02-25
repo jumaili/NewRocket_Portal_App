087d809ee0eadec539957e6ffe8e6f1caa0343b1
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-24 17:04:49 -0800

    Refactored the files for the components to start working again

--- app/pages/Home/home.module.js
+++ app/pages/Home/home.module.js
@@ -2,7 +2,7 @@ var home = angular.module('home',[]);
 
 // this is to create a custom directive, and we use camelCase in the js file while it'll be a snake case in the html
 angular.module('home').component('homeComponent', {
-  template: '<h1>something</h1>',
+  templateUrl: './pages/Home/homeView.html',
   controller: function($scope) {
     $scope.message = "Hi";
   }
