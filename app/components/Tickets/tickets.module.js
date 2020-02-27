ec109a6dab0c5fe31a7f1b2378a79953dd98d34d
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-26 23:13:24 -0800

    Able to create external and gave it fetched with an api call and have it displayed dynamically

--- app/components/Tickets/tickets.module.js
+++ app/components/Tickets/tickets.module.js
@@ -3,8 +3,19 @@ angular.module('tickets',[]);
 // this is to create a custom directive, and we use camelCase in the js file while it'll be a snake case in the html
 angular.module('tickets').component('ticketsComponent', {
   templateUrl: './components/Tickets/ticketsView.html',
-  controller: function($scope) {
-    $scope.message = "Tickets";
+  controller: function($http, $scope) {
+    // this is how to get data from an api
+  // recall that this is an asynchonous function
+  $http.get('./data/itTickets.json')
+  .then(function successCallback(response) {
+    $scope.ticketsData = response.data
+
+    console.log("Status:", response.statusText);
+    console.log($scope.ticketsData)
+    }, function errorCallback(response) {
+      console.log("Status:",response.statusText);
+    });;
+
   }
 });
 
