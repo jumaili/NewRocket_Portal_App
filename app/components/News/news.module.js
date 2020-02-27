3f16e1ca8b1ab7b5087cbe3be3104b604d146ecd
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-26 22:55:38 -0800

    Able to fetch the data api into a controller to have it displayed in the html for the newsView

--- app/components/News/news.module.js
+++ app/components/News/news.module.js
@@ -3,7 +3,17 @@ angular.module('news',[]);
 // this is to create a custom directive, and we use camelCase in the js file while it'll be a snake case in the html
 angular.module('news').component('newsComponent', {
   templateUrl: './components/News/newsView.html',
-  controller: function($scope) {
-    $scope.message = "News";
+  controller: function($http, $scope) {
+  // this is how to get data from an api
+  // recall that this is an asynchonous function
+  $http.get('./data/news.json')
+  .then(function successCallback(response) {
+    $scope.newsData = response.data
+
+    console.log("Status:", response.statusText);
+    console.log($scope.newsData)
+    }, function errorCallback(response) {
+      console.log("Status:",response.statusText);
+    });;
   }
 });
